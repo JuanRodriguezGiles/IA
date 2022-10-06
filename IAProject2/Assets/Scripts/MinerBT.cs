7e82b3e36c39e0a45d87081479bf8801aef05ab9
Juan <juanrodriguezgiles@gmail.com>
2022-10-06 19:40:30 -0300

    Import BT package.

---
+++ IAProject2/Assets/Scripts/MinerBT.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinerBT : Tree
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    protected override Node Setup()
+    {
+        Node root = new Root(new List<Node>
+        {
+            new Sequence(new List<Node>
+            {
+                new Not(new List<Node>{
+                //MyLeaveNode
+                }),
+                new Sequence(new List<Node>{
+                    new Not(new List<Node>(
+                        //MyLeaveNode2
+                        ))
+                    //MyLeaveNode3
+                })
+            })
+        });
+        return root;
+    }
+}
